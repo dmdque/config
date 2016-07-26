437f8e27c885240dcb00e317122630196ddee1bd
Daniel Que <dmdque@gmail.com>
2016-07-26 02:00:03 -0400

    Add script to install prezto.

---
+++ install_prezto.sh
@@ -0,0 +1,6 @@
+zsh
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+chsh -s /bin/zsh
